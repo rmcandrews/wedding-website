a32bc0036f721f58b12748c54ea69760d0a28798
Ryan McAndrews <rmcandre915@gmail.com>
2019-08-06 17:09:38 -0500

    Updates

---
+++ precache-manifest.b83c640fc91436a4b1eecd42cf301fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c923bc62615d2913414c49daf06563",
+    "url": "/wedding-website/index.html"
+  },
+  {
+    "revision": "8fc7acd9a253e0730118",
+    "url": "/wedding-website/static/css/2.bd39f990.chunk.css"
+  },
+  {
+    "revision": "f4bbd8865843eede6fad",
+    "url": "/wedding-website/static/css/main.bca21b27.chunk.css"
+  },
+  {
+    "revision": "8fc7acd9a253e0730118",
+    "url": "/wedding-website/static/js/2.1f812b2e.chunk.js"
+  },
+  {
+    "revision": "f4bbd8865843eede6fad",
+    "url": "/wedding-website/static/js/main.7fb04cee.chunk.js"
+  },
+  {
+    "revision": "59b515a577b7dd45478a",
+    "url": "/wedding-website/static/js/runtime~main.88091616.js"
+  }
+]);
