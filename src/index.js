4d292d6eb8d63e146de192c8e1c93a3c25650077
Ryan McAndrews <rmcandre915@gmail.com>
2019-11-20 20:30:39 -0600

    Change tag manager to js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import TagManager from "react-gtm-module";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+TagManager.initialize({
+  gtmId: "GTM-WGBRTR8"
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
