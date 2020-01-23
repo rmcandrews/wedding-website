78ce74979e3f61d958184b16d67a1fab19c4ba3a
Ryan McAndrews <rmcandre915@gmail.com>
2020-01-22 23:05:54 -0600

    Improve invitation overview style

--- src/pages/Admin/components/Invitation/Invitation.jsx
+++ src/pages/Admin/components/Invitation/Invitation.jsx
@@ -6,24 +6,33 @@ function Invitation({ invitation, handleEdit }) {
   let { address } = invitation;
 
   let displayedAddress = "";
+  let addressNeeded = false;
   if (address.line1) {
     displayedAddress = `${address.line1} ${
       address.line2 ? ` ${address.line2}` : ""
     }, ${address.city}, ${address.state} ${address.zip}`;
+  } else {
+    displayedAddress = "Address Needed";
+    addressNeeded = true;
   }
 
   return (
     <div className="invitation" style={{ overflow: "auto" }}>
       <div>
         <span className="title">
-          <span className="titleName">{invitation.name}</span>
-          <span className="titleAddress">{displayedAddress}</span>
+          <div className="titleName">{invitation.name}</div>
+          <div
+            className="titleAddress"
+            style={{ color: addressNeeded ? "red" : "" }}
+          >
+            {displayedAddress}
+          </div>
         </span>
         <Button
           size="mini"
           basic
           onClick={() => handleEdit(invitation.id)}
-          style={{ float: "right" }}
+          style={{ float: "right", marginRight: 0, marginTop: "0.9em" }}
         >
           View/Edit
         </Button>
